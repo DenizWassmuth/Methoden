761ec7c2946d21fdf5e8f3e5c666b5ef3e4b033b
Babuk GameDev <babuk.gamedev@gmail.com>
2025-12-02 11:53:23 +0100

    add Main Class

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public  static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+//        PrintString("\nErsten Summanden eingeben: ");
+//        int n1=input.nextInt();
+//        PrintString("\nZweiten Summanden eingeben: ");
+//        int n2=input.nextInt();
+//
+//        String OutPutString = "\nDas Ergebnis ist: " + Taschenrechner.Add(n1,n2);
+//        PrintString(OutPutString);
+
+
+    }
+
+    public static void PrintString(String str)
+    {
+        System.out.println(str);
+    }
+}
