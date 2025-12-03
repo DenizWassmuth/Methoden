5c9b8fc30575de0990677b2379cbc21ba66df1e2
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-03 21:12:22 +0100

    add Level 1 exercises

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,58 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // zu 1
+        printHello("Hello Welt");
+
+        // zu 2
+        printString(getGreeting());
+
+        // zu 3
+        halloUser("Deniz");
+
         Scanner input = new Scanner(System.in);
 
-        PrintString("\nErsten Summanden eingeben: ");
+        printString("\nErsten Summanden eingeben: ");
         int n1=input.nextInt();
-        PrintString("\nZweiten Summanden eingeben: ");
+        printString("\nZweiten Summanden eingeben: ");
         int n2=input.nextInt();
-        
+
         String OutPutString = "\nDas Ergebnis ist: " + Taschenrechner.Add(n1,n2);
-        PrintString(OutPutString);
+        printString(OutPutString);
     }
 
-    public static void PrintString(String str)
+    public static void printString(String str)
     {
         System.out.println(str);
     }
+
+
+// 1. Einfache Methode ohne Rückgabewert
+//    Erstelle eine Methode printHello, die "Hallo Welt" auf der Konsole ausgibt.
+//    Rufe die Methode in main auf.
+
+    public static void printHello(String text)
+    {
+        printString(text);
+    }
+
+
+// 2. Einfache Methode mit Rückgabewert
+//    Erstelle eine Methode getGreeting, die einen String zurückgibt: "Willkommen im Java-Kurs".
+//    Gib das Ergebnis in der main-Methode auf der Konsole aus.
+
+    public static String getGreeting()
+    {
+        return "Willkommen im Java-Kurs";
+    }
+
+
+// 3. Parameter verwenden
+//    Schreibe eine Methode printName, die einen Namen als Parameter erhält und "Hallo <Name>" ausgibt.
+
+    public static void halloUser(String userName)
+    {
+        printString("Hallo " + userName);
+    }
+
 }
